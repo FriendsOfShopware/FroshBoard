2e5676e30090978230caae6c9a3b48786046e76f
tinect <s.koenig@tinect.de>
2020-08-23 19:12:09 +0200

    order repos align repos create link onto assignee

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,21 @@ export default {
     }
   },
   async created() {
-    let res = await window.fetch('https://api.friendsofshopware.com/v2/github/repositories')
+    let res = await window.fetch('https://api.friendsofshopware.com/v2/github/repositories');
     this.repos = await res.json();
+    this.repos = this.repos.sort(function(a, b) {
+      const nameA = a.name.toUpperCase();
+      const nameB = b.name.toUpperCase();
+      if (nameA < nameB) {
+        return -1;
+      }
+
+      if (nameA > nameB) {
+        return 1;
+      }
+
+      return 0;
+    });
   }
 }
 </script>
--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,11 +1,8 @@
 <template>
-    <div v-if="issues" :id="repo.name">
+    <div v-if="issues" :id="repo.name" class="inline-block">
         <div class="flex m-4 justify-between">
             <div class="flex">
                 <h3 class="mr-4"><a :href="repo.html_url" target="_blank" class="text-white">{{ repo.name }}</a></h3>
-                <ul class="list-reset text-white hidden md:flex">
-                    <li><span class="font-bold text-lg px-2">☆</span></li>
-                </ul>
             </div>
         </div>
         <div class="flex px-4 pb-8 items-start overflow-x-scroll">
@@ -18,11 +15,13 @@
                         <h3 class="text-sm"><a :href="issue.html_url" class="text-black no-underline">#{{ issue.number }}</a></h3>
                         <a :href="issue.html_url" class="text-black no-underline break-words" target="_blank">{{ issue.title }}</a>
                         <div class="text-grey-darker mt-2 ml-2 flex justify-between items-start">
-                            <img v-if="issue.assignee" :src="issue.assignee.avatar_url" width="32" class="rounded-full" />
+                            <a v-if="issue.assignee" :href="issue.assignee.html_url" :title="issue.assignee.login" target="_blank">
+                              <img :src="issue.assignee.avatar_url" width="32" class="rounded-full" />
+                            </a>
                         </div>
 
                         <div class="my-2" v-if="issue.labels">
-                            <span class="text-sm font-medium py-1 px-2 rounded text-white align-middle" v-for="label in issue.labels" :style="{ 'background-color': '#' + label.color }">{{ label.name }}</span>
+                            <div class="text-sm font-medium py-1 px-2 rounded text-white align-middle m-1" v-for="label in issue.labels" :style="{ 'background-color': '#' + label.color }">{{ label.name }}</div>
                         </div>
 
                     </div>
