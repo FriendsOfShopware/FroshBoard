d904d2f70833592a3c16df93a9b90e5bd937509c
Soner Sayakci <s.sayakci@shopware.com>
2020-08-23 18:07:56 +0200

    Add labels

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="issues">
+    <div v-if="issues" :id="repo.name">
         <div class="flex m-4 justify-between">
             <div class="flex">
                 <h3 class="mr-4"><a :href="repo.html_url" target="_blank" class="text-white">{{ repo.name }}</a></h3>
@@ -15,10 +15,16 @@
                 </div>
                 <div class="text-sm mt-2">
                     <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter" v-for="issue in issues" v-bind:key="issue.id">
+                        <h3 class="text-sm"><a :href="issue.html_url" class="text-black no-underline">#{{ issue.number }}</a></h3>
                         <a :href="issue.html_url" class="text-black no-underline break-words" target="_blank">{{ issue.title }}</a>
                         <div class="text-grey-darker mt-2 ml-2 flex justify-between items-start">
                             <img v-if="issue.assignee" :src="issue.assignee.avatar_url" width="32" class="rounded-full" />
                         </div>
+
+                        <div class="my-2" v-if="issue.labels">
+                            <span class="text-sm font-medium py-1 px-2 rounded text-white align-middle" v-for="label in issue.labels" :style="{ 'background-color': '#' + label.color }">{{ label.name }}</span>
+                        </div>
+
                     </div>
                 </div>
             </div>
