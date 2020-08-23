eca3c46ce83ea83d0a44bcb9605dd80e3b460521
Soner Sayakci <s.sayakci@shopware.com>
2020-08-23 15:48:13 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app" class="w-full font-sans">
+    <Board v-for="repo in repos" :repo="repo" v-bind:key="repo.id"></Board>
+  </div>
+</template>
+
+<script>
+import Board from './components/Board';
+
+export default {
+  name: 'App',
+  components: {
+    Board
+  },
+  data() {
+    return {
+      repos: []
+    }
+  },
+  async created() {
+    let res = await window.fetch('https://api.friendsofshopware.com/v2/github/repositories')
+    this.repos = await res.json();
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Board.vue
@@ -0,0 +1,52 @@
+<template>
+    <div v-if="issues">
+        <div class="flex m-4 justify-between">
+            <div class="flex">
+                <h3 class="mr-4"><a :href="repo.html_url" target="_blank" class="text-white">{{ repo.name }}</a></h3>
+                <ul class="list-reset text-white hidden md:flex">
+                    <li><span class="font-bold text-lg px-2">☆</span></li>
+                </ul>
+            </div>
+        </div>
+        <div class="flex px-4 pb-8 items-start overflow-x-scroll">
+            <div class="rounded bg-grey-light  flex-no-shrink w-64 p-2 mr-3">
+                <div class="flex justify-between py-1">
+                    <h3 class="text-sm">Open issues ({{ issues.length }})</h3>
+                </div>
+                <div class="text-sm mt-2">
+                    <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter" v-for="issue in issues" v-bind:key="issue.id">
+                        <a :href="issue.html_url" class="text-black no-underline break-words" target="_blank">{{ issue.title }}</a>
+                        <div class="text-grey-darker mt-2 ml-2 flex justify-between items-start">
+                            <img v-if="issue.assignee" :src="issue.assignee.avatar_url" width="32" class="rounded-full" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Board',
+        props: {
+            repo: Object
+        },
+        data() {
+            return {
+                issues: []
+            }
+        },
+        async created() {
+            try {
+                let res = await window.fetch(`https://api.friendsofshopware.com/v2/github/issues/${this.repo.name}`)
+                this.issues = await res.json();
+            } catch (e) {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
